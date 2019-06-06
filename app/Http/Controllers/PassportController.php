ecad1a359e8fdce373e19fcbc124ebd7051fad2a
Kiran Bhattarai <kiranbhattarai2018@hotmail.com>
2019-06-06 11:54:07 -0500

    Custom class and methods created for http response

--- app/Http/Controllers/PassportController.php
+++ app/Http/Controllers/PassportController.php
@@ -2,9 +2,11 @@
 
 namespace App\Http\Controllers;
 
+use App\Classes\MyClasses;
 use App\Http\Resources\User as UserResource;
 use App\User;
 use Illuminate\Http\Request;
+use Validator;
 
 class PassportController extends Controller
 {
@@ -16,11 +18,16 @@ class PassportController extends Controller
      */
     public function register(Request $request)
     {
-        $this->validate($request, [
+        $httpResponse = new MyClasses();
+        $validator = Validator::make($request->all(), [
             'name' => 'required|min:3',
             'email' => 'required|email|unique:users',
-            'password' => 'required|min:6',
+            'password' => 'required|min:8',
+            'c_password' => 'required|same:password',
         ]);
+        if ($validator->fails()) {
+            return (new UserResource(['message' => 'Validation Error', 'details' => $validator->errors(), 'status' => $httpResponse->httpResponse(400)]))->response()->setStatusCode(400);
+        }
 
         $user = User::create([
             'name' => $request->name,
@@ -28,9 +35,9 @@ class PassportController extends Controller
             'password' => bcrypt($request->password),
         ]);
 
-        $token = $user->createToken('TutsForWeb')->accessToken;
+        $token = $user->createToken('kiranbhattarai.me')->accessToken;
 
-        return new UserResource($token);
+        return (new UserResource(['message' => 'Account Created', 'access_token' => $token, 'status' => $httpResponse->httpResponse(201)]))->response()->setStatusCode(201);
     }
 
     /**
@@ -45,12 +52,12 @@ class PassportController extends Controller
             'email' => $request->email,
             'password' => $request->password,
         ];
-
+        $httpResponse = new MyClasses();
         if (auth()->attempt($credentials)) {
             $token = auth()->user()->createToken('kiranbhattarai.me')->accessToken;
-            return new UserResource(['access_token' => $token], 200);
+            return (new UserResource(['message' => 'successfull login', 'access_token' => $token, 'status' => $httpResponse->httpResponse(200)]))->response()->setStatusCode(200);
         } else {
-            return response()->json(['error' => 'UnAuthorized'], 401);
+            return (new UserResource(['message' => 'Invalid email or password', 'status' => $httpResponse->httpResponse(401)]))->response()->setStatusCode(401);
         }
     }
 
